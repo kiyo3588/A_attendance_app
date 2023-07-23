43cd0a33e392bf9b7ae296652ab0e01fb50d3c3a
kiyo3588 <kiyoohara0627@email.com>
2023-07-24 08:53:58 +0900

    ユーザーカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230722123024) do
+ActiveRecord::Schema.define(version: 20230723234418) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20230722123024) do
     t.datetime "basic_time", default: "2023-07-21 23:00:00"
     t.datetime "work_time", default: "2023-07-21 22:30:00"
     t.boolean "superior", default: false
+    t.string "employee_number"
+    t.string "uid"
+    t.datetime "designated_work_start_time"
+    t.datetime "designated_work_end_time"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
