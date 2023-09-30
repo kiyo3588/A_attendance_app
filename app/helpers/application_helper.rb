4f6037344a095ff99ad9ac0b423a954b7ed73815
kiyo3588 <kiyoohara0627@email.com>
2023-09-30 09:03:34 +0900

    勤怠ログモーダル呼び出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+
+  def day_color_class(wday)
+    case wday
+    when 6 # 土曜日
+      "text-blue"
+    when 0 # 日曜日
+      "text-red"
+    else
+      ""
+    end
+  end
 end
